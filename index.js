ae4539d03051a398d28b1db76c376d6035cb15a3
Ziad003 <ziad.hossain15@gmail.com>
2025-11-14 16:18:40 +0600

    addedHistoryData

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ for(const heart of hearts){
     })
 }
 
+let historyData=[];
+
 let callBtns=document.getElementsByClassName('callBtn');
 for(let callBtn of callBtns){
 
@@ -15,16 +17,57 @@ for(let callBtn of callBtns){
         let name=parent.querySelector('p').innerText;
         let number=parent.querySelector('h1').innerText;
         
+        let title=parent.querySelector('h2').innerText;
         let coin=document.getElementById("coinCount");
         if(parseInt(coin.innerText)<20){
-            alert("❌ you don't have enough coin!")
+            alert("❌ you don't have enough coins!")
             return;
         }else{
             alert(`📞 calling ${name} ${number}...`)
 
             //coin cut
             coin.innerText=parseInt(coin.innerText)-20;
-        }
 
+            //Adding at history
+            let data={
+                name: title,
+                number: number,
+                time:new Date().toLocaleTimeString()
+
+            }
+            historyData.push(data);
+            historyFun();
+
+        }
    })
+}
+
+
+
+function historyFun(){
+    const history=document.getElementById('historyContainer');
+    history.innerText='';
+
+    for(let data of historyData){
+    const div=document.createElement('div');
+    div.innerHTML=`
+        <div id="histories" class="mt-2 flex justify-between items-center p-4 bg-[#fafafa] rounded-lg">
+                <div id="serviceInfo">
+                    <p id="serviceName" class="text-[18px] font-semibold">
+                        ${data.name}
+                    </p>
+                    <p class="serviceNumber text-gray-600 text-[18px]">
+                        ${data.number}
+                    </p>
+                </div>
+
+                <div id="serviceTime">
+                    <p class="text-[18px]">
+                        ${data.time}
+                    </p>
+                </div>
+            </div>
+    `
+    history.appendChild(div);
+}
 }
