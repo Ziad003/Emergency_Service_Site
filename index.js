e91b696502cb6a0c68d82b7f2751bbbb60de0e71
Ziad003 <ziad.hossain15@gmail.com>
2025-11-14 15:01:54 +0600

    coinCutFunctionality

--- index.js
+++ index.js
@@ -8,10 +8,23 @@ for(const heart of hearts){
 
 let callBtns=document.getElementsByClassName('callBtn');
 for(let callBtn of callBtns){
+
+    //Alert Show
    callBtn.addEventListener('click',function(){
         let parent=callBtn.parentElement.parentElement;
         let name=parent.querySelector('p').innerText;
         let number=parent.querySelector('h1').innerText;
-        alert(`📞 calling ${name} ${number}...`)
+        
+        let coin=document.getElementById("coinCount");
+        if(parseInt(coin.innerText)<20){
+            alert("❌ you don't have enough coin!")
+            return;
+        }else{
+            alert(`📞 calling ${name} ${number}...`)
+
+            //coin cut
+            coin.innerText=parseInt(coin.innerText)-20;
+        }
+
    })
 }
